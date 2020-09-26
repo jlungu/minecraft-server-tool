8f6d77522b6c391c307ddee537a0390a72a508bf
Ken Rose <32115575+AKKenn@users.noreply.github.com>
2020-09-26 15:35:43 -0400

    Instructions Page

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
 import Home from '../components/Home.vue';
 import Download from '../components/Download.vue';
+import Instructions from '../components/Instructions.vue';
 const routes = [
     { path: '/', component: Home },
     { path: '/download', component: Download },
+    { path: '/instructions', component: Instructions },
 ];
 
 export default routes;
